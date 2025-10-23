af5cc8afe5d3e62d5638c84f50f1d07bd3c12aa4
KhPankiv <khpankiv@gmail.com>
2025-10-23 20:36:12 +0200

    studio: UA desk with filters (Літургії/Оголошення)

--- deskStructure.js
+++ deskStructure.js
@@ -2,6 +2,34 @@
   S.list()
     .title('Контент')
     .items([
-      S.listItem().title('Статті').schemaType('article').child(S.documentTypeList('article').title('Статті')),
-      S.listItem().title('Розклад').schemaType('schedule').child(S.documentTypeList('schedule').title('Розклад')),
+      S.listItem()
+        .title('Статті')
+        .schemaType('article')
+        .child(S.documentTypeList('article').title('Статті')),
+
+      S.listItem()
+        .title('Розклад')
+        .child(
+          S.list()
+            .title('Розклад')
+            .items([
+              S.listItem()
+                .title('Усе')
+                .child(S.documentTypeList('schedule').title('Весь розклад')),
+              S.listItem()
+                .title('Літургії')
+                .child(
+                  S.documentTypeList('schedule')
+                    .title('Літургії')
+                    .filter('_type == "schedule" && category == "liturgy"')
+                ),
+              S.listItem()
+                .title('Оголошення')
+                .child(
+                  S.documentTypeList('schedule')
+                    .title('Оголошення')
+                    .filter('_type == "schedule" && category == "announcement"')
+                ),
+            ])
+        ),
     ])
